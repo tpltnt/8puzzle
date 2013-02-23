bb773e8969e56f35a3e465579000f81a657b4b2a
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 15:54:07 +0100

    search init done

--- forwardSearch.py
+++ forwardSearch.py
@@ -39,7 +39,9 @@ def forwardSearch(initialstate, goalstate):
     This function searches a path from a given initial state
     to the given goal state. It returns the path found.
     """
-    print("fwrdsrch called")
+
+    currentstate = initialstate
+    plan = []
 
 
 # only call if script is executed (and not included)
@@ -47,4 +49,3 @@ if __name__ == '__main__':
     goalstate = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     initalstate = [1, 6, 4, 8, 7, 0, 3, 2, 5]
     forwardSearch(initalstate,goalstate)
-    print("huha")
