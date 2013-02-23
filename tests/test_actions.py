1286a86f08988107e9b9ec41247c8bacddc94195
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 17:22:22 +0100

    tests fixed

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -7,21 +7,26 @@ from forwardSearch import *
 
 
 def test_moveLeft():
-    centercenter = [1,2,3,4,0,5,6,7,8]
-    centerleft = [1,2,3,0,4,5,6,7,8]
+    centercenter = State([1,2,3,4,0,5,6,7,8])
+    centerleft = State([1,2,3,0,4,5,6,7,8])
     assert centerleft == moveLeft(centercenter)
 
 def test_moveRight():
-    centercenter = [1,2,3,4,0,5,6,7,8]
-    centerright = [1,2,3,4,5,0,6,7,8]
+    centercenter = State([1,2,3,4,0,5,6,7,8])
+    centerright = State([1,2,3,4,5,0,6,7,8])
     assert centerright == moveRight(centercenter)
 
 def test_moveUp():
-    centercenter = [1,2,3,4,0,5,6,7,8]
-    centertop = [1,0,3,4,2,5,6,7,8]
+    centercenter = State([1,2,3,4,0,5,6,7,8])
+    centertop = State([1,0,3,4,2,5,6,7,8])
     assert centertop == moveUp(centercenter)
 
 def test_moveDown():
-    centercenter = [1,2,3,4,0,5,6,7,8]
-    centerbottom = [1,2,3,4,7,5,6,0,8]
+    centercenter = State([1,2,3,4,0,5,6,7,8])
+    centerbottom = State([1,2,3,4,7,5,6,0,8])
     assert centerbottom == moveDown(centercenter)
+
+def test_moveDown_impossible():
+    centerbottom = State([1,2,3,4,7,5,6,0,8])
+    with pytest.raises(RuntimeError):
+        moveDown(centerbottom)
