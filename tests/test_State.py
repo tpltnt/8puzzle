f2d245ae1c1c70a826754718033209a00eaea1f1
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 16:22:52 +0100

    tests for actions added

--- tests/test_State.py
+++ tests/test_State.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../8puzzle')
 import pytest
-from forwardSearch import State
+from forwardSearch import *
 
 
 def test_constructor_empty():
@@ -28,3 +28,26 @@ def test_isEmpty_notfalse():
     teststate = State()
     assert False != teststate.isEmpty()
 
+def test_getInternalState():
+    teststate = State([0,3,1,5,4,2,8,7,6])
+    assert [0,3,1,5,4,2,8,7,6] == teststate.getInternalState()
+
+def test_moveLeft():
+    centercenter = [1,2,3,4,0,5,6,7,8]
+    centerleft = [1,2,3,0,4,5,6,7,8]
+    assert centerleft == moveLeft(centercenter)
+
+def test_moveRight():
+    centercenter = [1,2,3,4,0,5,6,7,8]
+    centerright = [1,2,3,4,5,0,6,7,8]
+    assert centerright == moveRight(centercenter)
+
+def test_moveUp():
+    centercenter = [1,2,3,4,0,5,6,7,8]
+    centertop = [1,0,3,4,2,5,6,7,8]
+    assert centertop == moveUp(centercenter)
+
+def test_moveDown():
+    centercenter = [1,2,3,4,0,5,6,7,8]
+    centerbottom = [1,2,3,4,7,5,6,0,8]
+    assert centerbottom == moveDown(centercenter)
