c9ee1ed52b71e068b112da7fee2810b8c423656d
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 17:44:01 +0100

    typo fix

--- tests/test_forwardSearch.py
+++ tests/test_forwardSearch.py
@@ -8,7 +8,7 @@ from forwardSearch import *
 
 def test_forwardSearch_correct_call():
     foo = State([1,2,3,4,7,5,6,0,8])
-    bar = State([1,2,3,4,7,5,6,8.0])
+    bar = State([1,2,3,4,7,5,6,8,0])
     # returned plan should be just a moveLeft
     forwardSearch(foo,bar)
 
