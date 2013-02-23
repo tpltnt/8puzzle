4118f37181193ad8cb4d804ad74759fead579be5
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 14:36:47 +0100

    negativ test added

--- tests/test_State.py
+++ tests/test_State.py
@@ -5,3 +5,7 @@ from forwardSearch import State
 def test_isEmpty():
     teststate = State()
     assert True == teststate.isEmpty()
+
+def test_isEmpty_notfalse():
+    teststate = State()
+    assert False != teststate.isEmpty()
