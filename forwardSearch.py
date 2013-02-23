33dbf2870455f899d54538fb9eb5655069ba5ca6
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 13:16:50 +0100

    script execution vs. include catched + docstrings

--- forwardSearch.py
+++ forwardSearch.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
-print("yay, hello")
+def forwardSearch(initialstate, goalstate):
+    """Implementation for forward search in state space
+
+    This function searches a path from a given initial state
+    to the given goal state. It returns the path found.
+    """
+    print("fwrdsrch called")
+
+
+# only call if script is executed (and not included)
+if __name__ == '__main__':
+    goalstate = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    initalstate = [1, 6, 4, 8, 7, 0, 3, 2, 5]
+    forwardSearch(initalstate,goalstate)
+    print("huha")
