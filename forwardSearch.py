4ce8f1111988f7eb6b167c66b0cfeeadbb9eb405
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 17:32:10 +0100

    changes enforced

--- forwardSearch.py
+++ forwardSearch.py
@@ -80,6 +80,6 @@ def forwardSearch(initialstate, goalstate):
 
 # only call if script is executed (and not included)
 if __name__ == '__main__':
-    goalstate = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    initalstate = [1, 6, 4, 8, 7, 0, 3, 2, 5]
+    goalstate = State([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    initalstate = State([1, 6, 4, 8, 7, 0, 3, 2, 5])
     forwardSearch(initalstate,goalstate)
