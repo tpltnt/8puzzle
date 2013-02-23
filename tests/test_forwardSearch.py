b4b24859a9fd02ca813be70296aa4a0f6c75055f
tpltnt <tpltnt.github@dropcut.net>
2013-02-23 17:39:10 +0100

    first tests for forward search

---
+++ tests/test_forwardSearch.py
@@ -0,0 +1,17 @@
+# tests behavior of forward search
+
+import sys
+sys.path.append('../8puzzle')
+import pytest
+from forwardSearch import *
+
+
+def test_forwardSearch_correct_call():
+    foo = State([1,2,3,4,7,5,6,0,8])
+    bar = State([1,2,3,4,7,5,6,8.0])
+    # returned plan should be just a moveLeft
+    forwardSearch(foo,bar)
+
+def test_forwardSearch_same_state():
+    centerbottom = State([1,2,3,4,7,5,6,0,8])
+    assert [] == forwardSearch(centerbottom,centerbottom)
