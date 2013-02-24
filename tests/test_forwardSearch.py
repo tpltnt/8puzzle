4308ada81cc75eb11d9b564f262c9c652379ec0b
tpltnt <tpltnt.github@dropcut.net>
2013-02-24 10:52:25 +0100

    actual search test added

--- tests/test_forwardSearch.py
+++ tests/test_forwardSearch.py
@@ -10,11 +10,11 @@ def test_forwardSearch_correct_call():
     foo = State([1,2,3,4,7,5,6,0,8])
     bar = State([1,2,3,4,7,5,6,8,0])
     # returned plan should be just a moveLeft
-#    forwardSearch(foo,bar)
+    assert {0: moveLeft} == forwardSearch(foo,bar)
 
 def test_forwardSearch_same_state():
     centerbottom = State([1,2,3,4,7,5,6,0,8])
-    assert [] == forwardSearch(centerbottom,centerbottom)
+    assert {} == forwardSearch(centerbottom,centerbottom)
 
 def test_forwardSearch_first_type_wrong():
     with pytest.raises(TypeError):
