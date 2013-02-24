de43f9488888877b7179f9ee4034d3a42ed71cd5
tpltnt <tpltnt.github@dropcut.net>
2013-02-24 16:01:47 +0100

    test fixed

--- tests/test_forwardSearch.py
+++ tests/test_forwardSearch.py
@@ -9,8 +9,7 @@ from forwardSearch import *
 def test_forwardSearch_correct_call():
     foo = State([1,2,3,4,7,5,6,0,8])
     bar = State([1,2,3,4,7,5,6,8,0])
-    # returned plan should be just a moveLeft
-    assert {0: moveLeft} == forwardSearch(foo,bar)
+    assert {0: moveRight} == forwardSearch(foo,bar)
 
 def test_forwardSearch_same_state():
     centerbottom = State([1,2,3,4,7,5,6,0,8])
